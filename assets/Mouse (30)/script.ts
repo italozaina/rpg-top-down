201fc4a02728d13c0bf900123b209f6169d3bc23
Aloma <loh_mendrot@hotmail.com>
2017-09-21 17:21:00 -0300

    Start implmentation of click to walk

--- assets/Mouse (30)/script.ts
+++ assets/Mouse (30)/script.ts
@@ -3,12 +3,14 @@ class MouseBehavior extends Sup.Behavior {
   camera = Sup.Actor;  
   timer = 60;
   cordToWalk: Sup.Math.Vector2;
-  makeWalk: boolean;
+  makeWalkX: boolean;
+  makeWalkY: boolean;
   
   awake() {
     Game.mouse = this;
     Sup.log( Sup.Input.getScreenSize());
-    this.makeWalk = false;
+    this.makeWalkX = false;
+    this.makeWalkY = false;
   }
 
   update() {
@@ -30,28 +32,70 @@ class MouseBehavior extends Sup.Behavior {
     //TODO Click to walk
     if(Sup.Input.isMouseButtonDown(0)){
       this.cordToWalk = this.position.toVector2().multiplyScalar(10);
-      Sup.log("      -----     ");      
-      Sup.log("X: ",Math.floor(this.cordToWalk.x / 10));
-      Sup.log("Y: ",Math.floor(this.cordToWalk.y / 10)); 
-      this.makeWalk = true;
+      // Sup.log("      -----     ");      
+      // Sup.log("X: ",Math.floor(this.cordToWalk.x / 10));
+      // Sup.log("Y: ",Math.floor(this.cordToWalk.y / 10));
+      let pX = Math.floor(this.cordToWalk.x / 10);
+      let cX = Math.floor(Game.player.actor.getPosition().toVector2().multiplyScalar(10).x);
+      let pY = Math.floor(this.cordToWalk.y / 10);
+      let cY = Math.floor(Game.player.actor.getPosition().toVector2().multiplyScalar(10).y);        
+      this.makeWalkX = true;
+      this.makeWalkY = true;
+      
+      //TODO set Direction to look
+      if(cX > cY){
+        if(pX < cX){
+          //LEFT
+          Game.player.direction = "L";
+        } else {
+          //RIGTH
+          Game.player.direction = "R";
+        }
+      } else {
+        if(pY < cY){
+          //DOWN
+          Game.player.direction = "D";
+        } else {
+          //UP
+          Game.player.direction = "U";
+        }        
+      }
+      //TODO start animation walk
+      Game.player.idle = false;
+      Game.player.walk = true;
     }
     
     //TODO Continue walk to reach coods
-    if(this.makeWalk){
-      let pX = Math.floor(this.cordToWalk.x);
-      let cX = Math.floor(Game.player.actor.getPosition().toVector2().multiplyScalar(10).x);
-      if(pX != cX) {
+    if(this.makeWalkX){
+      let pX = Math.floor(this.cordToWalk.x / 10) * 10;
+      let cX = Math.floor(Game.player.actor.getPosition().toVector2().multiplyScalar(10).x);      
+      if(pX == cX) {
+        this.makeWalkX = false;
+        Game.player.actor.arcadeBody2D.setVelocityX(0); 
+      } 
         if(pX < cX) 
-          Game.player.actor.moveX(-0.1);       
+          Game.player.actor.arcadeBody2D.setVelocityX(-0.05);       
         if(pX > cX) 
-          Game.player.actor.moveX(0.1);
-      Sup.log("@@      -----     @@");      
-      Sup.log("X1: ",pX);              
-      Sup.log("X2: ",cX);              
-        // this.makeWalk = true;
-      } else {
-        this.makeWalk = false;
-      }
+          Game.player.actor.arcadeBody2D.setVelocityX(0.05);
+    }
+    
+    if(this.makeWalkY){
+      let pY = Math.floor(this.cordToWalk.y / 10) * 10;
+      let cY = Math.floor(Game.player.actor.getPosition().toVector2().multiplyScalar(10).y);       
+      if(pY == cY) {
+        this.makeWalkY = false;            
+        Game.player.actor.arcadeBody2D.setVelocityY(0);     
+      } 
+        if(pY < cY) 
+          Game.player.actor.arcadeBody2D.setVelocityY(-0.05);       
+        if(pY > cY) 
+          Game.player.actor.arcadeBody2D.setVelocityY(0.05);
+    }    
+    
+    //TODO stop animation
+    if(!this.makeWalkX && !this.makeWalkY){
+      Game.player.idle = true;
+      Game.player.walk = false;      
     }
     
     // if(this.timer === 0){
